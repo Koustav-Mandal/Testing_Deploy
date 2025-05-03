9d12a38cf66b031003ec62cc7b2e917ca7ae6a90
Koustav Mandal <125867117+Koustav-Mandal@users.noreply.github.com>
2025-05-04 02:54:30 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -4,7 +4,7 @@ import numpy as np
 #import seaborn as sns
 import string
 import re
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
