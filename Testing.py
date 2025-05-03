ce1493acbae78dd8310641ee8810c7edbb2710ff
Koustav Mandal <125867117+Koustav-Mandal@users.noreply.github.com>
2025-05-04 02:55:08 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -5,7 +5,7 @@ import numpy as np
 import string
 import re
 #from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+#from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
 data_fake=pd.read_csv(r"C:\Users\KOUSTAV\Downloads\Fake.csv")
